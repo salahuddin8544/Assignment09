69edb0604ac10b3f9c9b2f9e5f8fe32a4d7e99c1
salahuddin8544 <salahuddinahmed8544@gmail.com>
2022-10-10 22:05:32 +0600

    setup router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './layout/Main';
+import Home from './components/Home/Home';
 function App() {
+  const router = createBrowserRouter([
+    {
+      path:'/',
+      element:<Main></Main>,
+      children:[
+        {
+          path:'/',
+          element:<Home></Home>
+        }
+      ]
+    }
+  ])
   return (
     <div className="App">
-     <h1 className='text-danger'>Test Bootstrap</h1>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
