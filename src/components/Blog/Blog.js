eb1afed28632ab52d16717204e8e5a9bd18a535d
salahuddin8544 <salahuddinahmed8544@gmail.com>
2022-10-11 21:17:02 +0600

    added blog question

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,10 +1,20 @@
 import React from 'react';
-
 const Blog = () => {
     return (
-        <div>
-            
-        </div>
+        <div className='row'>
+          <div>
+            <h3>What is purpose of react-router?</h3>
+            <p>React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL.</p>
+          </div>
+          <div>
+            <h3>How does API works?</h3>
+            <p>React.createContext() is all you need. It returns a consumer and a provider. Provider is a component that as it's names suggests provides the state to its children. It will hold the "store" and be the parent of all the components that might need that store. Consumer as it so happens is a component that consumes and uses the state</p>
+          </div>
+          <div>
+            <h3>What is useRef Hook?</h3>
+            <p>The useRef is a hook that allows to directly create a reference to the DOM element in the functional component.The useRef returns a mutable ref object. This object has a property called .current. The value is persisted in the refContainer.current property</p>
+          </div>
+      </div>
     );
 };
 
--- src/components/SingleQuiz/SingleQuiz.js
+++ src/components/SingleQuiz/SingleQuiz.js
@@ -1,24 +1,31 @@
 import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const SingleQuiz = ({singleQuiz}) => {
     const {question,id,options,correctAnswer} = singleQuiz;
         const clicked = (option)=>{
             if(option === correctAnswer){
-
+             toast("Correct answer");
             }
             else{
-                console.log('wrong');
+                toast("Wrong answer");
             }
         }
-
+        const correct =()=>{
+            toast(`${correctAnswer}`)
+        }
     return (
         <div className='bg-success border m-4'>
-            <button>fav</button>
+          <button onClick={()=>correct()}>Correct</button>
             {question}
            <div className='text-warning'>
+           <div>
+      </div>
             {
                 options.map(option=> <button onClick={()=>clicked(option)}><input type="radio" /> {option} 
                 </button>)
             }
+            <ToastContainer />
            </div>
         </div>
     );
